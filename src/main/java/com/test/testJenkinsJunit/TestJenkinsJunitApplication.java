eefdbb2fa3e27fbde2eaed62191daee20683942d
Armando Perea <Armando.Perea@noaa.gov>
2018-11-26 11:00:43 -0500

    hi

---
+++ src/main/java/com/test/testJenkinsJunit/TestJenkinsJunitApplication.java
@@ -0,0 +1,12 @@
+package com.test.testJenkinsJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsJunitApplication.class, args);
+	}
+}
